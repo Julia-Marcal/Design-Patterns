ebd57e5a1cc4cc84313a41c72201f87a90b2559c
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-10-05 17:06:16 -0300

    cleaning main.go

--- main.go
+++ main.go
@@ -1,28 +1,7 @@
 package main
 
-import (
-	creational "github.com/Julia-Marcal/Design-Patterns/Creational"
-	structural "github.com/Julia-Marcal/Design-Patterns/Structural"
-)
-
 func main() {
-	board := "skate"
-	creational.CreateBoard(board)
-
-	carBuilder := creational.NewCarBuilder()
-	carBuilder.Year = 2000
-	carBuilder.Model = "Eclipse"
-	carBuilder.Make = "Mitsubishi"
-	carBuilder.Color = "Green"
-
-	creditSystem := &structural.RealCreditSystem{}
-	debitSystem := &structural.RealDebitSystem{}
-
-	facade := structural.PaymentFacade{
-		Credit: creditSystem,
-		Debit:  debitSystem,
-	}
-
-	facade.ProcessPayment(structural.Credit, 100)
-	facade.ProcessPayment(structural.Debit, 50)
+	CreationalDesignPatterns()
+	StructuralDesignPatterns()
+	BehavioralDesignPatterns()
 }
